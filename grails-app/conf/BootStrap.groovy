c3fdc5b83eb4395b33206d977aaee3f3d00ffbdb
Ricardo Jorge Tavares da Silva <ricardo.silva@petrotec.pt>
2014-11-19 15:02:34 +0000

    Beyond Scaffolding - Configuring a Database

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,75 +1,82 @@
+import grails.util.Environment
+
 import com.tekdays.*
+
+
 class BootStrap {
 	def init = { servletContext ->
+		if (Environment.getCurrent().equals(Environment.DEVELOPMENT)) {
+			if (!TekEvent.get(1)) {
+				new TekUser(fullName: 'John Doe',
+				userName: 'jdoe',
+				password: 't0ps3cr3t',
+				email: 'jdoe@johnsgroovyshop.com',
+				website: 'blog.johnsgroovyshop.com',
+				bio: 'John has been programming for over 40 years. ...').save()
 
-		new TekUser(fullName: 'John Doe',
-		userName: 'jdoe',
-		password: 't0ps3cr3t',
-		email: 'jdoe@johnsgroovyshop.com',
-		website: 'blog.johnsgroovyshop.com',
-		bio: 'John has been programming for over 40 years. ...').save()
+				new TekUser(fullName: 'John Deere',
+				userName: 'tractorman',
+				password: 't0ps3cr3t',
+				email: 'john.deere@porkproducers.org',
+				website: 'www.perl.porkproducers.org',
+				bio: 'John is a top notch Perl programmer and a ...').save()
 
-		new TekUser(fullName: 'John Deere',
-		userName: 'tractorman',
-		password: 't0ps3cr3t',
-		email: 'john.deere@porkproducers.org',
-		website: 'www.perl.porkproducers.org',
-		bio: 'John is a top notch Perl programmer and a ...').save()
-
-		def event1 = new TekEvent(name: 'Gateway Code Camp',
-		city: 'Saint Louis, MO',
-		organizer: TekUser.findByFullName('John Doe'),
-		venue: 'TBD',
-		startDate: new Date('11/21/2013'),
-		endDate: new Date('11/21/2013'),
-		description: '''This conference will bring
+				def event1 = new TekEvent(name: 'Gateway Code Camp',
+				city: 'Saint Louis, MO',
+				organizer: TekUser.findByFullName('John Doe'),
+				venue: 'TBD',
+				startDate: new Date('11/21/2013'),
+				endDate: new Date('11/21/2013'),
+				description: '''This conference will bring
 						coders ...''').save()
 
-		def event2 = new TekEvent(name: 'Perl Before Swine',
-		city: 'Austin, MN',
-		organizer: TekUser.findByFullName('John Deere'),
-		venue: 'SPAM Museum',
-		startDate: new Date('11/2/2013'),
-		endDate: new Date('11/2/2013'),
-		description: 'Join the Perl programmers of the ...').save()
+				def event2 = new TekEvent(name: 'Perl Before Swine',
+				city: 'Austin, MN',
+				organizer: TekUser.findByFullName('John Deere'),
+				venue: 'SPAM Museum',
+				startDate: new Date('11/2/2013'),
+				endDate: new Date('11/2/2013'),
+				description: 'Join the Perl programmers of the ...').save()
 
-		def g1 = TekEvent.findByName('Gateway Code Camp')
-		g1.addToVolunteers(new TekUser(fullName: 'Sarah Martin',
-		userName: 'sarah',
-		password: '54321',
-		email: 'sarah@martinworld.com',
-		website: 'www.martinworld.com',
-		bio: 'Web designer and Grails afficianado.'))
+				def g1 = TekEvent.findByName('Gateway Code Camp')
+				g1.addToVolunteers(new TekUser(fullName: 'Sarah Martin',
+				userName: 'sarah',
+				password: '54321',
+				email: 'sarah@martinworld.com',
+				website: 'www.martinworld.com',
+				bio: 'Web designer and Grails afficianado.'))
 
-		g1.addToVolunteers(new TekUser(fullName: 'Bill Smith',
-		userName: 'Mr_Bill',
-		password: '12345',
-		email: 'mrbill@email.com',
-		website: 'www.mrbillswebsite.com',
-		bio: 'Software developer, claymation artist.'))
-		g1.addToRespondents('ben@grailsmail.com')
-		g1.addToRespondents('zachary@linuxgurus.org')
-		g1.addToRespondents('solomon@bootstrapwelding.com')
-		g1.save()
+				g1.addToVolunteers(new TekUser(fullName: 'Bill Smith',
+				userName: 'Mr_Bill',
+				password: '12345',
+				email: 'mrbill@email.com',
+				website: 'www.mrbillswebsite.com',
+				bio: 'Software developer, claymation artist.'))
+				g1.addToRespondents('ben@grailsmail.com')
+				g1.addToRespondents('zachary@linuxgurus.org')
+				g1.addToRespondents('solomon@bootstrapwelding.com')
+				g1.save()
 
-		def s1 = new Sponsor(name:'Contegix',
-		website:'http://www.contegix.com',
-		description:'Beyond Managed Hosting for your Enterprise').save()
+				def s1 = new Sponsor(name:'Contegix',
+				website:'http://www.contegix.com',
+				description:'Beyond Managed Hosting for your Enterprise').save()
 
-		def s2 = new Sponsor(name:'Object Computing Incorporated',
-		website:'http://ociweb.com',
-		description:'An OO Software Engineering Company'
-		).save()
-		
-		def sp1 = new Sponsorship(event:g1,
-		sponsor:s1,
-		contributionType:'Other',
-		description:'Cool T-Shirts').save()
-		
-		def sp2 = new Sponsorship(event:g1,
-		sponsor:s2,
-		contributionType:'Venue',
-		description:'Will be paying for the Moscone').save()
+				def s2 = new Sponsor(name:'Object Computing Incorporated',
+				website:'http://ociweb.com',
+				description:'An OO Software Engineering Company'
+				).save()
+
+				def sp1 = new Sponsorship(event:g1,
+				sponsor:s1,
+				contributionType:'Other',
+				description:'Cool T-Shirts').save()
+
+				def sp2 = new Sponsorship(event:g1,
+				sponsor:s2,
+				contributionType:'Venue',
+				description:'Will be paying for the Moscone').save()
+			}
+		}
 	}
 	def destroy = {
 	}
